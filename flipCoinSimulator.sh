87084efec3a89b16f2719c48c2a03c009e445873
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-08 00:49:32 +0530

    Flip Coin and printing result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,11 @@
-#! /bin/bash -x
+
 echo " Welcome to Flip-Coin Simulator "
+flip=$((($RANDOM%2)+1))
+if [ $flip -eq 1 ]
+then
+        echo "head"
+        ((h++))
+else
+        echo "tail"
+        ((t++))
+fi
