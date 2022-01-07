e6e5bd5263ead4cf02e5f22c0d5a872f39e13197
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-08 00:59:05 +0530

    Flipping Coin multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,19 @@
 
 echo " Welcome to Flip-Coin Simulator "
-flip=$((($RANDOM%2)+1))
-if [ $flip -eq 1 ]
-then
-        echo "head"
-        ((h++))
-else
-        echo "tail"
-        ((t++))
-fi
+read -p "Enter a number to check if its Prime : " number
+h=0
+t=0
+for ((i=0;i<$number;i++))
+do
+        flip=$((($RANDOM%2)+1))
+        if [ $flip -eq 1 ]
+        then
+                echo "head"
+                ((h++))
+        else
+                echo "tail"
+                ((t++))
+        fi
+done
+echo "Heads wins: " $h "Tail wins: " $t
+
